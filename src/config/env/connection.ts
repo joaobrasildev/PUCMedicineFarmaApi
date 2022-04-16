a4f7850b54563c705819101923f87f5fd3eb00b6
Joao Brasil <jvobsystems@gmail.com>
2022-04-16 13:38:28 -0300

    container config

--- src/config/env/connection.ts
+++ src/config/env/connection.ts
@@ -7,16 +7,27 @@ dotenv.config();
 export const config: TypeOrmModuleOptions = {
   type: 'postgres',
   host: env().application.host,
-  port: env().application.dbPort ? parseInt(env().application.dbPort) : 5442,
+  port: env().application.dbPort ? parseInt(env().application.dbPort) : 5432,
   username: env().application.username,
   password: env().application.password,
   database: env().application.database,
   synchronize: false,
 };
 
+export const configString: TypeOrmModuleOptions = {
+  url: process.env.DATABASE_URL,
+  type: 'postgres',
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  synchronize: true, // This for development
+  autoLoadEntities: true,
+}
+
 export function getDatabaseConfigConnection(): TypeOrmModuleOptions {
   return {
-    ...config,
+    ...configString,
     entities: ['dist/shared/database/entities/*.entity.js'],
   };
 }
